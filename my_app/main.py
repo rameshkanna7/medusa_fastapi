7c984dad71bdb7763230c724208e7e16b2df1a8f
unknown <rameshkanna@aurexus.com>
2023-11-02 15:55:19 +0530

    sql

--- my_app/main.py
+++ my_app/main.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Header, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
+from typing import Optional
 app = FastAPI()
 templates = Jinja2Templates(directory=Path("my_app") / "templates")
 app.mount("/static", StaticFiles(directory=Path("my_app") / "static"), name="static")
@@ -18,7 +18,9 @@ data = [
 
 
 @app.get("/index", response_class=HTMLResponse)
-async def root_page(request: Request):
+async def root_page(request: Request, hx_request: Optional[str] = Header(None)):
+    if hx_request:
+        return templates.TemplateResponse("table.html", context={"request": request, "lib": data})
     return templates.TemplateResponse(
         "index.html", context={"request": request, "lib": data}
     )
